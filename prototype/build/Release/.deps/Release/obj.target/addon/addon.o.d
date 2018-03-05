6fdc8c965186c83d5e830f3e4e8058c1051cf104
U-TOSHIBA-TSDV\quannm <quannm@QUANNM.toshiba-tsdv.com>
2018-03-05 18:55:50 +0700

    first commit

---
+++ prototype/build/Release/.deps/Release/obj.target/addon/addon.o.d
@@ -0,0 +1,13 @@
+cmd_Release/obj.target/addon/addon.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/0.12.6/src -I/home/pi/.node-gyp/0.12.6/deps/uv/include -I/home/pi/.node-gyp/0.12.6/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/addon/addon.o.d.raw  -c -o Release/obj.target/addon/addon.o ../addon.cc
+Release/obj.target/addon/addon.o: ../addon.cc \
+ /home/pi/.node-gyp/0.12.6/src/node.h \
+ /home/pi/.node-gyp/0.12.6/deps/v8/include/v8.h \
+ /home/pi/.node-gyp/0.12.6/deps/v8/include/v8stdint.h \
+ /home/pi/.node-gyp/0.12.6/deps/v8/include/v8config.h \
+ /home/pi/.node-gyp/0.12.6/src/node_version.h
+../addon.cc:
+/home/pi/.node-gyp/0.12.6/src/node.h:
+/home/pi/.node-gyp/0.12.6/deps/v8/include/v8.h:
+/home/pi/.node-gyp/0.12.6/deps/v8/include/v8stdint.h:
+/home/pi/.node-gyp/0.12.6/deps/v8/include/v8config.h:
+/home/pi/.node-gyp/0.12.6/src/node_version.h:
---
+++ prototype/public/js/sockethandle.js
@@ -0,0 +1,6 @@
+﻿var socket = io();
+socket.on('chat message', function (msg) {
+    $("#temperature").html(msg.temp);
+    $("#humidity").html(msg.humi);
+    $("#distance").html(msg.dis);
+});
